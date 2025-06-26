7950a32f29b565ed05081739d64669e982934b58
Tomasz Rudnicki <tomek198821@wp.pl>
2025-06-26 15:59:53 +0100

    header and sidebar added for desktop and mobile

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,9 +1,17 @@
+import { Header } from 'components';
 import React from 'react'
 
 const AllUsers = () => {
   return (
-    <div>Users Table</div>
-  )
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips Page"
+        description="Check out our current users in real time"
+      />
+
+      All Users Page Contents
+    </main>
+  );
 }
 
 export default AllUsers
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,7 +1,18 @@
+import { Header } from "components"
+
+
 const Dashboard = () => {
+  const user = { name: 'Tom' };
+
   return (
-    <div>Dashboard page</div>
-  )
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"} 👋✈️ `}
+        description="Track activity, trends and popular destinations in real time"
+      />
+      Dashboard Page Contents
+    </main>
+  );
 }
 
 export default Dashboard
