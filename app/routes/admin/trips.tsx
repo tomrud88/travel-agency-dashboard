b7a4f756a259de70ff15e1233ab3666460c9942a
Tomasz Rudnicki <tomek198821@wp.pl>
2025-07-01 10:45:36 +0100

    All Trips implemented

--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -1,16 +1,88 @@
-import { Header } from "components"
+import { Header, TripCard } from "components"
+import { useSearchParams, type LoaderFunctionArgs } from "react-router";
+import { getAllTrips } from "~/appwrite/trips";
+import { parseTripData } from "~/lib/utils";
+import type { Route } from "./+types/trips";
+import { useState } from "react";
+import { PagerComponent } from "@syncfusion/ej2-react-grids";
 
-const Trips = () => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const limit = 4;
+  const url = new URL(request.url)
+  const page = parseInt(url.searchParams.get('page') || "1", 10);
+  const offset = (page - 1) * limit;
+
+  const { allTrips, total } = await getAllTrips(limit, offset);
+
+  return {
+    trips:allTrips.map(({ $id, tripDetail, imageUrls }) => ({
+      id: $id,
+      ...parseTripData(tripDetail),
+      imageUrls: imageUrls ?? [],
+    })),
+    total
+  };
+};
+
+const Trips = ({ loaderData }: Route.ComponentProps) => {
+  const trips = loaderData.trips as Trip[] | [];
+
+  const [searchParams] = useSearchParams();
+  const initialPage = Number(searchParams.get('page') || '1');
+
+  const [currentPage, setCurrentPage] = useState(initialPage);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    window.location.search = `?page=${page}`
+  }
+
+
   return (
     <main className="all-users wrapper">
-    <Header
-      title="Trips"
-              description="View and edit AI-generated travel plans"
-              ctaText="Create a trip"
-              ctaUrl="/trips/create"
-    />
+      <Header
+        title="Trips"
+        description="View and edit AI-generated travel plans"
+        ctaText="Create a trip"
+        ctaUrl="/trips/create"
+      />
+
+      <section>
+        <h1 className="p-24-semibold text-dark-100 mb-4">Manage Created Trips</h1>
+        <div className="trip-grid mb-4">
+          {trips.map(
+            ({
+              id,
+              name,
+              imageUrls,
+              itinerary,
+              interests,
+              travelStyle,
+              estimatedPrice,
+            }) => (
+              <TripCard
+                id={id}
+                key={id}
+                name={name}
+                location={itinerary?.[0].location ?? ""}
+                imageUrl={imageUrls[0]}
+                tags={[interests, travelStyle]}
+                price={estimatedPrice}
+              />
+            )
+          )}
+        </div>
+
+        <PagerComponent
+          totalRecordsCount={loaderData.total}
+          pageSize={4}
+          currentPage={currentPage}
+          click={(args) => handlePageChange(args.currentPage)}
+          className="!mb-4"
+        />
+      </section>
     </main>
-  )
-}
+  );
+};
 
 export default Trips
