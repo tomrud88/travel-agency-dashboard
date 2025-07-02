117a15cbdfbc2d9c88c0c73c72a35bc7c757fd11
Tomasz Rudnicki <tomek198821@wp.pl>
2025-07-02 11:10:34 +0100

    connecting real data and added charts to the dashboard

--- app/appwrite/dashboard.ts
+++ app/appwrite/dashboard.ts
@@ -1,86 +1,166 @@
-import { appwriteConfig, database } from "./client";
+import { parseTripData } from "~/lib/utils";
+import { database, appwriteConfig } from "./client";
 
 interface Document {
-    [key:string]: any,
+  [key: string]: any;
 }
 
-type filterByDate = (
-    items: Document[],
-    key: string,
-    start: string,
-    end?: string,
+type FilterByDate = (
+  items: Document[],
+  key: string,
+  start: string,
+  end?: string
 ) => number;
 
-export const getUserAndTripsStats = async (): Promise<DashboardStats> => {
-    const d = new Date();
-    const startCurrent = new Date(d.getFullYear(), d.getMonth(), 1).toISOString();
-    const startPrev = new Date(d.getFullYear(),d.getMonth() -1, 1).toISOString();
-    const endPrev = new Date(d.getFullYear(), d.getMonth(), 0).toISOString();
-    
-    const [users, trips] = await Promise.all([
-      database.listDocuments(
-        appwriteConfig.databaseId,
-        appwriteConfig.userCollectionId
+export const getUsersAndTripsStats = async (): Promise<DashboardStats> => {
+  const d = new Date();
+  const startCurrent = new Date(d.getFullYear(), d.getMonth(), 1).toISOString();
+  const startPrev = new Date(
+    d.getFullYear(),
+    d.getMonth() - 1,
+    1
+  ).toISOString();
+  const endPrev = new Date(d.getFullYear(), d.getMonth(), 0).toISOString();
+
+  const [users, trips] = await Promise.all([
+    database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId
+    ),
+    database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.tripCollectionId
+    ),
+  ]);
+
+  const filterByDate: FilterByDate = (items, key, start, end) =>
+    items.filter((item) => item[key] >= start && (!end || item[key] <= end))
+      .length;
+
+  const filterUsersByRole = (role: string) => {
+    return users.documents.filter((u: Document) => u.status === role);
+  };
+
+  return {
+    totalUsers: users.total,
+    usersJoined: {
+      currentMonth: filterByDate(
+        users.documents,
+        "joinedAt",
+        startCurrent,
+        undefined
       ),
-      database.listDocuments(
-        appwriteConfig.databaseId,
-        appwriteConfig.tripCollectionId
+      lastMonth: filterByDate(users.documents, "joinedAt", startPrev, endPrev),
+    },
+    userRole: {
+      total: filterUsersByRole("user").length,
+      currentMonth: filterByDate(
+        filterUsersByRole("user"),
+        "joinedAt",
+        startCurrent,
+        undefined
       ),
-    ]);
+      lastMonth: filterByDate(
+        filterUsersByRole("user"),
+        "joinedAt",
+        startPrev,
+        endPrev
+      ),
+    },
+    totalTrips: trips.total,
+    tripsCreated: {
+      currentMonth: filterByDate(
+        trips.documents,
+        "createdAt",
+        startCurrent,
+        undefined
+      ),
+      lastMonth: filterByDate(
+        filterUsersByRole("user"),
+        "joinedAt",
+        startPrev,
+        endPrev
+      ),
+    },
+  };
+};
 
-    const filterByDate: filterByDate = (items, key, start, end) => items.filter((item) => (
-        item[key] >= start && (!end || item[key] <= end)
-)).length;
+export const getUserGrowthPerDay = async () => {
+  const users = await database.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.userCollectionId
+  );
 
-    const filterUsersByRole = (role: string) => {
-        return users.documents.filter((u: Document) => u.status === role)
-    }
+  const userGrowth = users.documents.reduce(
+    (acc: { [key: string]: number }, user: Document) => {
+      const date = new Date(user.joinedAt);
+      const day = date.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+      });
+      acc[day] = (acc[day] || 0) + 1;
+      return acc;
+    },
+    {}
+  );
 
-    return {
-      totalUsers: users.total,
-      usersJoined: {
-        currentMonth: filterByDate(
-          users.documents,
-          "joinedAt",
-          startCurrent,
-          undefined
-        ),
-        lastMonth: filterByDate(
-          users.documents,
-          "joinedAt",
-          startPrev,
-          endPrev
-        ),
-      },
-      userRole: {
-        total: filterUsersByRole("user").length,
-        currentMonth: filterByDate(
-          filterUsersByRole("user"),
-          "joinedAt",
-          startCurrent,
-          undefined
-        ),
-        lastMonth: filterByDate(
-          filterUsersByRole("user"),
-          "joinedAt",
-          startPrev,
-          endPrev
-        ),
-      },
-      totalTrips: trips.total,
-      tripsCreated: {
-        currentMonth: filterByDate(
-          trips.documents,
-          "createdAt",
-          startCurrent,
-          undefined
-        ),
-        lastMonth: filterByDate(
-          trips.documents,
-          "createdAt",
-          startPrev,
-          endPrev
-        ),
-      },
-    };
-}
+  return Object.entries(userGrowth).map(([day, count]) => ({
+    count: Number(count),
+    day,
+  }));
+};
+
+export const getTripsCreatedPerDay = async () => {
+  const trips = await database.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.tripCollectionId
+  );
+
+  const tripsGrowth = trips.documents.reduce(
+    (acc: { [key: string]: number }, trip: Document) => {
+      const date = new Date(trip.createdAt);
+      const day = date.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+      });
+      acc[day] = (acc[day] || 0) + 1;
+      return acc;
+    },
+    {}
+  );
+
+  return Object.entries(tripsGrowth).map(([day, count]) => ({
+    count: Number(count),
+    day,
+  }));
+};
+
+export const getTripsByTravelStyle = async () => {
+  const trips = await database.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.tripCollectionId
+  );
+
+  console.log("Raw trips data:", trips.documents);
+
+  const travelStyleCounts = trips.documents.reduce(
+    (acc: { [key: string]: number }, trip: Document) => {
+      const tripDetail = parseTripData(trip.tripDetail); 
+
+      if (tripDetail && tripDetail.travelStyle) {
+        const travelStyle = tripDetail.travelStyle;
+        acc[travelStyle] = (acc[travelStyle] || 0) + 1;
+      }
+      return acc;
+    },
+    {}
+  );
+
+  console.log("Travel style counts:", travelStyleCounts);
+  const result = Object.entries(travelStyleCounts).map(([travelStyle, count]) => ({
+    count: Number(count),
+    travelStyle,
+  }));
+  
+  return result;
+};
